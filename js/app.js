24eedea8ffe305dd3bf0343a470424570f81b00e
unknown <zeiadsamir33@gmail.com>
2022-05-08 16:09:09 +0200

    first

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const ad = document.getElementById('advice');
+const adId = document.getElementById('adId');
+const btn = document.getElementById('btn');
+
+console.log(ad,btn)
+
+btn.addEventListener('click', ()=> {
+    getAdvice();
+});
+
+function getAdvice(){
+    fetch('https://api.adviceslip.com/advice').then(response =>{
+    return response.json().then(adviceData => {
+        const adObj = adviceData.slip;
+        ad.innerHTML= `&#8220 ${adObj.advice} &#8221`;
+        adId.innerHTML= `${adObj.id}`;
+    })
+}).catch(error =>{
+    console.log(error)
+})
+}
